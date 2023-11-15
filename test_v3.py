b0b4c9685fa57969a8c10aaf56e95aa44f592a12
nikit34 <permikov134@mail.ru>
2023-11-15 22:03:06 +0000

    refactoring

--- test_v3.py
+++ test_v3.py
@@ -14,7 +14,7 @@ X = [
 labels = np.array([0, 1, 0, 0])
 
 Y = [1, 1, 1, 0, 0, 0]
-test = np.array([1,1,0,1,0,1,0,1])
+test = np.array([1, 1, 0, 1, 0, 1, 0, 1])
 
 perceptron = Perceptron()
 wt_matrix = perceptron.fit(X, labels, 1000, 0.01)
