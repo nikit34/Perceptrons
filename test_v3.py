464d6beeb3a7e3543e561bf8d49c99fd483c80c6
nikit34 <permikov134@mail.ru>
2023-11-16 02:27:02 +0000

    Fix v3

--- test_v3.py
+++ test_v3.py
@@ -11,10 +11,10 @@ X = [
     [0,0,0,1,1,0,0,0]
 ]
 
-labels = np.array([0, 1, 0, 0])
+labels = [0, 1, 0, 0, 0, 1]
 
 Y = [1, 1, 1, 0, 0, 0]
-test = np.array([1, 1, 0, 1, 0, 1, 0, 1])
+test = [1, 1, 0, 1, 0, 1]
 
 perceptron = Perceptron()
 wt_matrix = perceptron.fit(X, labels, 1000, 0.01)
--- v3.py
+++ v3.py
@@ -8,17 +8,17 @@ class Perceptron:
         self.b = None
 
     def model(self, x):
-        return 1 if (np.dot(self.w, x) >= self.b) else 0
+        return 1 if (np.dot(self.w, x).all() >= self.b) else 0
 
     def predict(self, X):
         Y = []
         for x in X:
             result = self.model(x)
             Y.append(result)
-        return np.array(Y)
+        return np.asarray(Y)
 
     def fit(self, X, Y, epochs=1, lr=1):
-        self.w = np.ones(X.shape[1])
+        self.w = np.ones(np.asarray(X).shape[1])
         self.b = 0
         accuracy = {}
         max_accuracy = 0
@@ -28,10 +28,10 @@ class Perceptron:
             for x, y in zip(X, Y):
                 y_pred = self.model(x)
                 if y == 1 and y_pred == 0:
-                    self.w = self.w + lr * x
+                    self.w = self.w + lr * np.asarray(x)
                     self.b = self.b - lr * 1
                 elif y == 0 and y_pred == 1:
-                    self.w = self.w - lr * x
+                    self.w = self.w - lr * np.asarray(x)
                     self.b = self.b + lr * 1
 
             wt_matrix.append(self.w)
@@ -48,4 +48,4 @@ class Perceptron:
         plt.ylabel('Accuracy')
         plt.ylim([0, 1])
         plt.show()
-        return np.array(wt_matrix)
+        return np.asarray(wt_matrix)
